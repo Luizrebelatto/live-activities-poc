db5ba852885d39f9af557c1643c3b64cec121d25
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-05-27 23:32:26 -0300

    init setup to live activities

---
+++ ios/Live Activity/Live_ActivityBundle.swift
@@ -0,0 +1,17 @@
+//
+//  Live_ActivityBundle.swift
+//  Live Activity
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 27/05/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Live_ActivityBundle: WidgetBundle {
+    var body: some Widget {
+        Live_Activity()
+        Live_ActivityLiveActivity()
+    }
+}
