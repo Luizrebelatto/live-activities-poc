50b95c9231c63190fe9454a0fe89f46f4f87f087
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-05-28 23:26:40 -0300

    create files to bridge react native

---
+++ ios/Live Activity/RCTLiveActivityModule.m
@@ -0,0 +1,8 @@
+//
+//  RCTLiveActivityModule.m
+//  liveactivitiespoc
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 28/05/25.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/Live Activity/nativeModules-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  nativeModules-Bridging-Header.h
+//  liveactivitiespoc
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 28/05/25.
+//
+
+#ifndef nativeModules_Bridging_Header_h
+#define nativeModules_Bridging_Header_h
+
+
+#endif /* nativeModules_Bridging_Header_h */
